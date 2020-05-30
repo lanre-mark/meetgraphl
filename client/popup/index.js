7f8995bfdc1e1404f6791835a64e6c9867eb9d4d
olanrewaju Makinde <lanre.developer@gmail.com>
2020-05-30 21:49:40 +0100

    refactored sidebar action

--- client/popup/index.js
+++ client/popup/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as browser from 'webextension-polyfill';
 import './index.css';
-// import Sidebar from './Sidebar';
 import App from '../extension/app';
+
+browser.runtime.sendMessage({ data: 'hello' });
 
 ReactDOM.render(<App />, document.getElementById('root'));
