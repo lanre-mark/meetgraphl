bf21a7e09e1bf8b4741ce78e3daf9fdc1c5faff5
olanrewaju Makinde <lanre.developer@gmail.com>
2020-05-30 20:52:36 +0100

    theme presets adaptation from react-analog-clock

---
+++ client/extension/theming/presets.js
@@ -0,0 +1,89 @@
+export const lightPreset = {
+  background: "#fff",
+  border: "#ececec",
+  center: "#000",
+  seconds: "#f56c6c",
+  minutes: "#ccc",
+  hour: "#000",
+  tick: "#000",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
+
+export const darkPreset = {
+  background: "#000",
+  border: "#000",
+  center: "#fff",
+  seconds: "#fff",
+  minutes: "#ccc",
+  hour: "#fff",
+  tick: "#fff",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
+
+export const aquaPreset = {
+  background: "#eaeaea",
+  border: "#3dd4c1",
+  center: "#000",
+  seconds: "#f56c6c",
+  minutes: "#9c9c9c",
+  hour: "#000",
+  tick: "#000",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
+
+export const limePreset = {
+  background: "#a4f181",
+  border: "#fff",
+  center: "#ccc",
+  seconds: "#fff",
+  minutes: "#ccc",
+  hour: "#fff",
+  tick: "#fff",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
+
+export const sherbertPreset = {
+  background: "linear-gradient(to left, #fee , #ddefbb)",
+  border: "#fff",
+  center: "#fff",
+  seconds: "#fff",
+  minutes: "#ccc",
+  hour: "#fff",
+  tick: "#fff",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
+
+export const navyPreset = {
+  background: "linear-gradient(#2a70a0,#102d42)",
+  border: "#fff",
+  center: "#fff",
+  seconds: "#fff",
+  minutes: "#ccc",
+  hour: "#fff",
+  tick: "#fff",
+  smallTickWidth: 2,
+  largeTickWidth: 4,
+  secondHandWidth: 3,
+  minuteHandWidth: 6,
+  hourHandWidth: 8,
+};
