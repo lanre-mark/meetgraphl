528e09d65506a2655d2dd1e6c72c7ee5b964b39a
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-05 18:33:47 +0100

    fixed commuincation between background and backend via sockets

--- client/background/index.js
+++ client/background/index.js
@@ -1,49 +1,82 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
+import {
+  initiateBrowserCall,
+  mutateConnState,
+  // lclMeetDict,
+  communicationType,
+  participantSocket,
+} from '../msg-comm/communicate';
 import io from 'socket.io-client';
 
 const socket = io('https://meetgrapheek.glitch.me');
 
-socket.on('connect', async () => {
-  console.log('Connected to the Socket Now');
-  commMessage = true;
-  // connectionStatus = messagePayloads.CONNECTED;
-  // const packet = {
-  //   type: messageTypes.CONNECTION_CHANGE,
-  //   payload: connectionStatus,
-  // };
-  try {
-    // await sendChromeMessage(packet);
-  } catch (err) {}
+initiateBrowserCall();
+
+socket.on('connect', async (client) => {
+  mutateConnState(true);
 });
 
 socket.on('disconnect', async () => {
   console.log('Disconnected from the Socket Now');
-  commMessage = false;
+  mutateConnState(false);
   // connectionStatus = messagePayloads.DISCONNECTED;
   // const packet = {
   //   type: messageTypes.CONNECTION_CHANGE,
   //   payload: connectionStatus,
   // };
-  try {
-    // await sendChromeMessage(packet);
-  } catch (err) {}
+  // try {
+  //   // await sendChromeMessage(packet);
+  // } catch (err) {}
 });
 
 function dispatchToServer(message) {
-  console.log('Dispatching message to the Server');
-  socket.emit('messageTypes.SEND_MESSAGE', message);
+  socket.emit('SEND_MESSAGE', message);
+}
+
+function dispatchParticipants(message) {
+  socket.emit(message.messageType, message.payload);
 }
 
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('request :: ', request);
-  console.log('sender :: ', sender);
-  console.log('sendResponse :: ', sendResponse);
-  switch (
-    request.type
-    // case
-    // default:
-  ) {
+  // console.log('Background sender browser.runtime.onMessage :: ', request);
+  const { payload } = request;
+  switch (request.messageType) {
+    case communicationType.SETUP_COMM:
+      console.log('Now we have been connected');
+      if (payload && payload['cnf-event-id']) {
+        socket.on(
+          payload['cnf-event-id'].split('spaces/')[1],
+          async (payload) => {
+            console.log(
+              `Received Meeting/Participant Payload as thus ::`,
+              payload
+            );
+          }
+        );
+      }
+      sendResponse({});
+      break;
+    case communicationType.ALL_DISCOVERED:
+      if (Array.isArray(payload)) {
+        const idDetails = payload
+          .filter((p) => p.isLocal === true)[0]
+          .id.split('/');
+        if (!participantSocket(`${idDetails[1]}_${idDetails[3]}`)) {
+          console.log(`Listenening on ${idDetails[1]}_${idDetails[3]}`);
+          socket.on(`${idDetails[1]}_${idDetails[3]}`, async (payload) => {
+            console.log(
+              `Received Meeting/Participant Payload as thus ::`,
+              payload
+            );
+          });
+        }
+        dispatchParticipants(request);
+      }
+      sendResponse({});
+      break;
+    default:
+      break;
   }
 });
