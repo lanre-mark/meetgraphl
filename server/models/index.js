71caed23c07f53a446e6301cccca159fe833f79f
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-12 04:11:08 +0100

    added weatherkeycontroller model for admistering the next waether api key to use to make weather calls. this hopes to integrate multiple api keys with a free quota bearing in mind the limitation of the keys within the space of 1min/60sec or as the case might be

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,11 @@
 const Conference = require('./conferences');
 const IpLocation = require('./iplocation');
 const ParticpantsSchema = require('./participants');
+const WeatherKeyController = require('./weatherkeycontroller');
 
-module.exports = { Conference, ParticpantsSchema, IpLocation };
+module.exports = {
+  Conference,
+  ParticpantsSchema,
+  IpLocation,
+  WeatherKeyController,
+};
---
+++ server/models/weatherkeycontroller.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const miscUtilities = require('../utility/misc');
+
+const weatherKeyControllerSchema = new mongoose.Schema(
+  {
+    apiKey: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+weatherKeyControllerSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 60 } // lifetime of 1 minute
+);
+
+weatherKeyControllerSchema.statics.nextAPIKey = async function() {
+  let lstkey = await WeatherKeyController.aggregate([
+    {
+      $group: {
+        _id: '$apiKey',
+        lastUsage: { $last: '$createdAt' },
+        usage: { $sum: 1 },
+      },
+    },
+    { $sort: { lastUsage: -1 } },
+    { $limit: 1 },
+  ]);
+  // console.log('Last apiKey data :: ', lstkey);
+  if (lstkey.length) {
+    // be sure that the number of the last one and the new one to be returned is within limit of the number of calls per API key within the expected limit
+    // count the number of records
+    if (lstkey[0].usage + 1 < process.env.WEATHER_API_SIZE_RANGE - 5) {
+      // console.log('Use the current max :: ', lstkey[0]._id);
+      return lstkey[0]._id;
+    } else {
+      // console.log('Use the one after the max');
+      return miscUtilities.NextKeyInSequence(lstkey[0]._id);
+    }
+  } else {
+    // use the first one
+    // console.log('Use the first one');
+    return miscUtilities.NextKeyInSequence();
+  }
+};
+
+const WeatherKeyController = mongoose.model(
+  'WeatherKeyController',
+  weatherKeyControllerSchema
+);
+
+module.exports = WeatherKeyController;
